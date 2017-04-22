d76cfa3cadb4b302798616df1f2217f614d0ce85
TalanL <Talan.L.Lousignont@gmail.com>
2017-04-21 23:57:01 -0600

    First iteration of gravity around a planet.

---
+++ FrisbeeWorld/Assets/Scripts/Gravity/GravityAffected.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class GravityAffected : MonoBehaviour
+{
+    public GravitySource source;
+    public bool grounded;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        source.Pull(this);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 10)
+        {
+            grounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 10 && grounded)
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ FrisbeeWorld/Assets/Scripts/Gravity/GravitySource.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravitySource : MonoBehaviour {
+    //why false?
+    bool useLocalUP = false;
+
+    float gravityAcceleration = -10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Pull(GravityAffected body)
+    {
+        Vector3 gravityUp;
+        Vector3 localUp;
+        Vector3 localForward;
+
+        Transform trans = body.transform;
+        Rigidbody rigidBody = body.GetComponent<Rigidbody>();
+
+        if (useLocalUP)
+        {
+            gravityUp = transform.up;
+        } else
+        {
+            gravityUp = trans.position - transform.position;
+            gravityUp.Normalize();
+        }
+
+        rigidBody.AddForce(gravityUp * gravityAcceleration * rigidBody.mass);
+        rigidBody.drag = (body.grounded) ? 1 : 0.1f;
+
+        if(rigidBody.freezeRotation)
+        {
+            localUp = trans.up;
+            Quaternion rotation = Quaternion.FromToRotation(localUp, gravityUp);
+            rotation = rotation * trans.rotation;
+            trans.rotation = Quaternion.Slerp(trans.rotation, rotation, 0.1f);
+            localForward = trans.forward;
+        }
+    }
+}
